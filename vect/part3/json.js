6eb5351f1d2036aebd73abed818366b44fe225ad
kamiya garg <kamiya.garg@gmail.com>
2016-08-08 15:23:05 -0700

    Assignment 11

---
+++ vect/part3/json.js
@@ -0,0 +1,27 @@
+{"employees":[
+	{
+		"name":"Agnes",
+		"employeeID":"E00A1",
+        "desp":"Agnes is the most seasoned and diversed member of the Vecta Corp."
+	},
+	{
+		"name":"Damon",
+		"employeeID":"E00A2",
+		"desp":"Damon is the Director of Development for Vecta Corp. Damon creates learning materials for Vecta Corp."
+	},
+	{
+		"name":"Herbert",
+		"employeeID":"E00B3",
+		"desp":"Herbert joined Vecta Corp. He has overall human resources responsibility for Vecta Corp's operations worldwide."
+	},
+	{
+		"name":"Mike",
+		"employeeID":"E00B4",
+		"desp":"Mike serves as the Vice President of Sales and Marketing for Vecta Corp. In this role Mike oversees Vecta Corp's marketing and corporate communications efforts worldwide."
+	},
+	{
+		"name":"Wilbur",
+		"employeeID":"E00B5",
+		"desp":"Wilbur is the founder and CEO of Vecta Corp, he is primarily known for being the pioneer and world leader of creating vSolutions."
+	}
+]}
